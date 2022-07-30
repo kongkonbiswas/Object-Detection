4860e7d02723788fcc6b52cca813e2731f2a87a2
Kongkon Biswas <kongkonbiswas3241@gmail.com>
2022-07-30 23:28:59 +0600

    video shown in frame sucessfuly

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Object-Detection
+# Object_DetectionDetection
+# For Getting Full Source Code & Tutorial Go Here:
+https://youtube.com/playlist?list=PLWyN7K28ZraTMKr5_240juiNEjo-IoenB
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import cv2
-import matplotlib.pyplot as plt
+video = cv2.VideoCapture("object.mp4")
 
+while True:
+    ret, frame = video.read()
+    cv2.imshow("Frame", frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+video.release()
+cv2.desrtoyAllWindows()
